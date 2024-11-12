a4d5efe09397b29213cd66a3fea93a9028a8418f
abdullahrabi <abdullahrabi026@gmail.com>
2024-11-13 00:34:58 +0500

    Add more Functionality

---
+++ my-react-app/src/Components/Assests/data/all_product.js
@@ -0,0 +1,76 @@
+import p10_img from './G1.jpg'
+import p11_img from './G2.jpg'
+import p12_img from './G3.jpg'
+import p13_img from './G4.jpg'
+import p14_img from './G5.jpg'
+import p15_img from './G6.jpg'
+import p16_img from './G7.jpg'
+let data_product = [
+    {
+        id:10,
+        name:"Sugar",
+        category:"Grocery",
+        image:p10_img,
+        new_price: 160,
+        old_price:180,
+    
+    },
+    
+    {
+        id:11,
+        name:"Pink Salt",
+        category:"Grocery",
+        image:p11_img,
+        new_price: 80,
+        old_price:90,
+    
+    },
+    
+    {
+        id:12,
+        name:"Flour(Atta)",
+        category:"Grocery",
+        image:p12_img,
+        new_price: 715,
+        old_price:725,
+    
+    },
+    {
+        id:13,
+        name:"Red Chilli Powder",
+        category:"Grocery",
+        image:p13_img,
+        new_price: 250,
+        old_price:260,
+    
+    },
+    {
+        id:14,
+        name:"Olivola Cooking Oil",
+        category:"Grocery",
+        image:p14_img,
+        new_price: 590,
+        old_price:620,
+    
+    },
+    {
+        id:15,
+        name:"Olivola Cooking Oil",
+        category:"Grocery",
+        image:p15_img,
+        new_price: 450,
+        old_price:460,
+    
+    },
+    {
+        id:16,
+        name:"Biryani Masala",
+        category:"Grocery",
+        image:p16_img,
+        new_price: 450,
+        old_price:460,
+    
+    },
+    
+    ];
+    export default all_product;
