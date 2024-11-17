e59ec144ddafe3abd5ec889b32f3f6007e66ed53
abdullahrabi <abdullahrabi026@gmail.com>
2024-11-18 04:23:43 +0500

    Remove login with Apple ID and Facebook

--- my-react-app/src/Pages/LoginSignup.jsx
+++ my-react-app/src/Pages/LoginSignup.jsx
@@ -1,9 +1,7 @@
 import React from 'react';
 import './CSS/LoginSignup.css';
 import loginImage from '../Components/Assests/Login.jpg'; 
-import facebook from '../Components/Assests/facebook.png'; 
 import google from '../Components/Assests/google.jpg'; 
-import apple from '../Components/Assests/apple.png'; 
 const LoginSignup = () => {
   return (
     <div className="loginsignup-page">
@@ -23,9 +21,9 @@ const LoginSignup = () => {
           <button className="login-button">Login</button>
           <p className="loginsignup-or">OR</p>
           <span className="social-login">
-            <a href="#"> <img src={facebook} alt="" /><i className="fab fa-facebook"></i></a>
+           
             <a href="#"><img src={google} /><i className="fab fa-google"></i></a>
-            <a href="#"><img src={apple} /><i className="fab fa-twitter"></i></a>
+          
           </span>
         </span>
       </span>
