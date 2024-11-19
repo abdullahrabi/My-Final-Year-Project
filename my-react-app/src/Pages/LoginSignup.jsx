5d28251ca0b50ce80627fe882b07c26296eb3fd7
abdullahrabi <abdullahrabi026@gmail.com>
2024-11-20 01:26:24 +0500

    Final the Login and Signup Page

--- my-react-app/src/Pages/LoginSignup.jsx
+++ my-react-app/src/Pages/LoginSignup.jsx
@@ -1,35 +1,51 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './CSS/LoginSignup.css';
 import loginImage from '../Components/Assests/Login.jpg'; 
 import google from '../Components/Assests/google.jpg'; 
+
 const LoginSignup = () => {
+  const [isSwapped, setIsSwapped] = useState(false); // state to track whether the form is swapped
+
+  const handleToggle = () => {
+    setIsSwapped(!isSwapped); 
+  };
+
   return (
-    <div className="loginsignup-page">
-     
+    <div className={`loginsignup-page ${isSwapped ? 'swapped' : ''}`}>
       <span className="loginsignup-left">
         <span className="loginsignup-container">
-          <h1>Login</h1>
+          <h1>{isSwapped ? 'Sign Up' : 'Login'}</h1>
           <span className="loginsignup-fields">
             <input type="email" placeholder="Email" />
             <input type="password" placeholder="Password" />
+            {isSwapped && (
+              <>
+                <input type="text" placeholder="Username" />
+                <input type="password" placeholder="Confirm Password" />
+              </>
+            )}
           </span>
           <span className='loginsignup-remember'>
             <input type="checkbox" />
             <label>Remember me</label>
             <a href="#">Forgot your password?</a>
           </span>
-          <button className="login-button">Login</button>
+          <button className="login-button">{isSwapped ? 'Sign Up' : 'Login'}</button>
+          <button className="toggle-button" onClick={handleToggle}>
+        {isSwapped ? 'Already have an account? Login here' : 'Don’t have an account? Sign up here'}
+      </button>
           <p className="loginsignup-or">OR</p>
           <span className="social-login">
-           
-            <a href="#"><img src={google} /><i className="fab fa-google"></i></a>
-          
+            <a href="#"><img src={google} alt="Google login" /><i className="fab fa-google"></i></a>
           </span>
         </span>
       </span>
-    <span className="loginsignup-right">
-        <img src={loginImage} alt="Nature background" />
+
+      <span className="loginsignup-right">
+        <img src={loginImage} alt="Our Store Welcome Page" />
       </span>
+
+    
     </div>
   );
 };
